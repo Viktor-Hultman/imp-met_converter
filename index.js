b59b94407cb3d0c8136f2a50895a6a4d1cd5621a
Viktor Hultman <60355017+Viktor-aj8092@users.noreply.github.com>
2021-10-13 15:31:20 +0200

    I added all the code which converts the distances

--- index.js
+++ index.js
@@ -44,20 +44,88 @@ function getToMetricDistVal() {
     console.log(toMetricDistValue);
 }
 
+//If statements for calculating the "Metric to Imperial convertion"
 metToImpBtn.addEventListener("click", function() {
-    if(metricNumValue < 1) {
+    if(metricNumValue <= 0) {
         console.log("error");
-    } else if(metricDistValue == "Centimeter" && toImperialDistValue == "Inch"){
+    } else if(metricDistValue == "Centimeter" && toImperialDistValue == "Inch") {
         result = metricNumValue / 2.54
+        console.log(result);
+    } else if(metricDistValue == "Centimeter" && toImperialDistValue == "Foot") {
+        result = metricNumValue * 0.032808
+        console.log(result);
+    } else if(metricDistValue == "Centimeter" && toImperialDistValue == "Yard") {
+        result = metricNumValue * 0.010936
+        console.log(result);
+    } else if(metricDistValue == "Centimeter" && toImperialDistValue == "Mile") {
+        result = metricNumValue * (62137/10000000000)
+        console.log(result);
+    } else if(metricDistValue == "Meter" && toImperialDistValue == "Inch") {
+        result = metricNumValue * 39.3700787402
+        console.log(result);
+    } else if(metricDistValue == "Meter" && toImperialDistValue == "Foot") {
+        result = metricNumValue * 3.280839895
+        console.log(result);
+    } else if(metricDistValue == "Meter" && toImperialDistValue == "Yard") {
+        result = metricNumValue * 1.0936132983
+        console.log(result);
+    } else if(metricDistValue == "Meter" && toImperialDistValue == "Mile") {
+        result = metricNumValue * 0.0006213712
+        console.log(result);
+    } else if(metricDistValue == "Kilometer" && toImperialDistValue == "Inch") {
+        result = metricNumValue * 39370.078740157
+        console.log(result);
+    } else if(metricDistValue == "Kilometer" && toImperialDistValue == "Foot") {
+        result = metricNumValue * 3280.8398950131
+        console.log(result);
+    } else if(metricDistValue == "Kilometer" && toImperialDistValue == "Yard") {
+        result = metricNumValue * 1093.6132983377
+        console.log(result);
+    } else if(metricDistValue == "Kilometer" && toImperialDistValue == "Mile") {
+        result = metricNumValue *  0.6213711922
         console.log(result);
     }
 })
 
+//If statements for calculating the "Imperial to Metric convertion"
 impToMetBtn.addEventListener("click", function() {
     if(imperialNumValue < 1) {
         console.log("error");
     } else if(imperialDistValue == "Inch" && toMetricDistValue == "Centimeter") {
         result = imperialNumValue * 2.54
+        console.log(result);
+    } else if(imperialDistValue == "Foot" && toMetricDistValue == "Centimeter") {
+        result = imperialNumValue * 30.48
+        console.log(result);
+    } else if(imperialDistValue == "Yard" && toMetricDistValue == "Centimeter") {
+        result = imperialNumValue * 91.44
+        console.log(result);
+    } else if(imperialDistValue == "Mile" && toMetricDistValue == "Centimeter") {
+        result = imperialNumValue * 160934.4
+        console.log(result);
+    } else if(imperialDistValue == "Inch" && toMetricDistValue == "Meter") {
+        result = imperialNumValue * 0.0254
+        console.log(result);
+    } else if(imperialDistValue == "Foot" && toMetricDistValue == "Meter") {
+        result = imperialNumValue * 0.3048
+        console.log(result);
+    } else if(imperialDistValue == "Yard" && toMetricDistValue == "Meter") {
+        result = imperialNumValue * 0.9144
+        console.log(result);
+    } else if(imperialDistValue == "Mile" && toMetricDistValue == "Meter") {
+        result = imperialNumValue * 1609.344
+        console.log(result);
+    } else if(imperialDistValue == "Inch" && toMetricDistValue == "Kilometer") {
+        result = imperialNumValue * 0.0000254
+        console.log(result);
+    } else if(imperialDistValue == "Foot" && toMetricDistValue == "Kilometer") {
+        result = imperialNumValue * 0.0003048
+        console.log(result);
+    } else if(imperialDistValue == "Yard" && toMetricDistValue == "Kilometer") {
+        result = imperialNumValue * 0.0009144
+        console.log(result);
+    } else if(imperialDistValue == "Mile" && toMetricDistValue == "Kilometer") {
+        result = imperialNumValue * 1.609344
         console.log(result);
     }
 })
